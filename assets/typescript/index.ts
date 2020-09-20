b141347b1f76361e62532e8d008e980af9ba3ece
David Winterbottom <david.winterbottom@gmail.com>
2020-09-20 14:51:10 +0100

    Add a map view that shows school admission radiuses from 2019
    Using a template found on Google Maps.

--- assets/typescript/index.ts
+++
@@ -1,2 +0,0 @@
-let color: string = "blue"
-alert(color)
---
+++ assets/typescript/index.tsx
@@ -0,0 +1,82 @@
+interface School {
+  center: google.maps.LatLngLiteral;
+  name: string;
+  radius_in_miles: float;
+}
+
+const schools: Record<string, School> = {
+  edmonton_county: {
+    center: { lat: 51.63528, lng: -0.06758 },
+    name: "Edmonton County",
+    radius_in_miles: 1.145
+  },
+  kingsmead: {
+    center: { lat: 51.65137, lng: -0.06122 },
+    name: "Kingsmead",
+    radius_in_miles: 0.916
+  },
+  winchmore: {
+    center: { lat: 51.62921, lng: -0.09142 },
+    name: "Winchmore",
+    radius_in_miles: 0.836
+  },
+  highlands: {
+    center: { lat: 51.64872, lng: -0.10816 },
+    name: "Highlands",
+    radius_in_miles: 0.843
+  },
+  ashmole: {
+    center: { lat: 51.63269, lng: -0.13618 },
+    name: "Ashmole",
+    radius_in_miles: 0.4
+  },
+  southgate: {
+    center: { lat: 51.64694, lng: -0.14074 },
+    name: "Southgate",
+    radius_in_miles: 1.996
+  },
+};
+
+function initMap(): void {
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 13,
+      // Bush Hill Park - via https://www.freemaptools.com/convert-uk-postcode-to-lat-lng.htm
+      center: { lat: 51.6365, lng: -0.06955 },
+      mapTypeId: "terrain"
+    }
+  );
+
+  for (const school in schools) {
+    const schoolCircle = new google.maps.Circle({
+      map,
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      center: schools[school].center,
+      // Convert miles to metres.
+      radius: schools[school].radius_in_miles * 1609,
+    });
+    const schoolMarker = new google.maps.Marker({
+      map,
+      position: schools[school].center,
+      title: schools[school].name,
+      label: schools[school].name,
+    });
+  }
+
+  // A marker for where we live
+  new google.maps.Marker({
+      map,
+      position: { lat: 51.6365, lng: -0.06955 },
+      icon: {
+          url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+      }
+  });
+}
+
+console.log(initMap);
+
